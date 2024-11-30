fcfd0705ef2ad231e8309d9b0c0ee7ed78ab832e
Vaibhav7567 <vaibhav7567@gmail.com>
2024-11-30 12:34:48 +0530

    after changes

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -5,7 +5,7 @@ function useCurrencyInfo(currency)
     const [data, setData] = useState({})
 
     useEffect(()=>{
-        fetch(``)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
     }, [currency])
